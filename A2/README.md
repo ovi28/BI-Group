22636faaad069fb3af4156131927e7ba5491b4ad
Ovi28 <ovi282@gmail.com>
2017-09-18 09:55:00 +0200

    2

---
+++ A2/README.md
@@ -0,0 +1,10 @@
+## How many sales records are there in total?  635391
+
+## For which zip code area do you have the most sales records?  2300
+## How many sales records? 17405
+## How big is the corresponding CSV file in kilobytes? 1753KB
+
+
+## For which zip code area do you have the fewest sales records? 7270
+## How many sales records? 133
+## How big is the corresponding CSV file in megabytes? 0.012 MB
--- A2/a2.py
+++ A2/a2.py
@@ -3,34 +3,26 @@ import requests
 import csv
 import os
 
-def run():
-    out_dir = './data/out'
-    if not os.path.exists(out_dir):
-        os.mkdir(out_dir)
-    
-    base_url = 'http://138.197.184.35/boliga/'
-    urls = ['1050-1549_1.html', '1050-1549_2.html']
-    urls = [os.path.join(base_url, url) for url in urls]
-
-    fst_fourty_results = scrape(urls[0])
-    snd_fourty_results = scrape(urls[1])
-    fst_results = fst_fourty_results + snd_fourty_results
-
-    save_to_file = os.path.join(out_dir, os.path.basename(urls[0]).split('.')[0] + '.csv')
-    save_to_csv(fst_results, save_to_file)
-
+def scrape_the_index():
+    data = []
+    r = requests.get("http://138.197.184.35/boliga/")
+    soup = bs4.BeautifulSoup(r.content.decode('utf-8'), 'html5lib')
+    for link in soup.find_all('a', href=True)[5:]:
+      if link.has_attr('href'):
+            data.append(link['href'])
+    return data
 
 def save_to_csv(data, path='./out/boliga.csv'):
-    
+    print(path)
     with open(path, 'w', encoding='utf-8') as output_file:
         output_writer = csv.writer(output_file)
         output_writer.writerow(['address','zip_code','price','sell_date',
                                 'sell_type','price_per_sq_m','no_rooms',
                                 'housing_type','size_in_sq_m','year_of_construction',
                                 'price_change_in_pct'])
-
         for row in data:
             output_writer.writerow(row)
+            
 
 def scrape(url):
     data = []
@@ -49,12 +41,20 @@ def scrape(url):
         # Decode address column
         address_str = cols[0].text.strip()
         address_str = address_str.replace("<br />"," ")
-        zip_number = int(address_str.split(' ')[-3])
+        if address_str.split(' ')[-3].isdigit():
+            zip_number = int(address_str.split(' ')[-3])
+        else:
+            zip_number = 0
+       
         
         # Decode price
         price_str = cols[1].text.strip()
         price_str = price_str.replace(".","")
-        price = float(price_str)
+        
+        if price_str.isdigit():
+            price = float(price_str)
+        else: 
+            price = 0
         
         # Decode sales date
         sale_date_str = cols[2].text.strip()
@@ -64,22 +64,35 @@ def scrape(url):
          # Decode sales date
         price_per_sq_mr_str = cols[3].text.strip()
         price_per_sq_mr_str = price_per_sq_mr_str.replace(".","")
-        price_per_sq_mr = int(price_per_sq_mr_str)
+        if price_per_sq_mr_str.isdigit():
+            price_per_sq_mr = int(price_per_sq_mr_str)
+        else: 
+            price_per_sq_mr = 0
         
         # Decode number of rooms
         no_rooms_str = cols[4].text.strip()
-        no_rooms = int(no_rooms_str)
+        if no_rooms_str.isdigit():
+            no_rooms = int(no_rooms_str)
+        else: 
+            no_rooms = 0
         
         # Decode housing type
         housing_type = cols[5].text.strip()
         
         # Decode selling date and type
         size_in_sq_m_str = cols[6].text.strip()
-        size_in_sq_m = int(size_in_sq_m_str)
+        if size_in_sq_m_str.isdigit():
+            size_in_sq_m = int(size_in_sq_m_str)
+        else: 
+            size_in_sq_m = 0
+       
 
         # Decode year of construction
         year_of_construction_str = cols[7].text.strip()
-        year_of_construction = int(year_of_construction_str)
+        if year_of_construction_str.isdigit():
+            year_of_construction = int(year_of_construction_str)
+        else: 
+            year_of_construction = 0
         
          # Price change in pct
         price_change_in_pct = cols[8].text.strip()
@@ -91,3 +104,33 @@ def scrape(url):
         data.append(decoded_row)
     
     return data
+    
+    
+    
+def run():
+    out_dir = './data/out'
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    
+    base_url = 'http://138.197.184.35/boliga/'
+    urls = scrape_the_index()
+    urls = [os.path.join(base_url, url) for url in urls]
+    info = []
+    x = 0
+    link_zip = ""
+    for link in urls:
+        print(link)
+        if link.split("_")[1] == "1.html":
+        
+            print("getting here")
+            if len(info)>0:
+                 if len(link_zip) < 1:
+                      link_zip = link
+                 save_to_file = os.path.join(out_dir, os.path.basename(link_zip.split('_')[0] + '.csv'))
+                 save_to_csv(info, save_to_file)
+                 link_zip = link
+                 info = []
+        info = info + scrape(link)
+    save_to_file = os.path.join(out_dir, os.path.basename(link_zip.split('_')[0] + '.csv'))
+    save_to_csv(info, save_to_file)
+   
